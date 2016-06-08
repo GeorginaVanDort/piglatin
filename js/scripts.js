338f6489739a016289ff6232c7b07249b679c36f
Georgina Van Dort and Julia Dickey <georgina@>
2016-06-08 15:54:00 -0700

    Show the returned phrase in the results div

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,34 @@
 $(document).ready(function() {
-
-
     $("form#pig").submit(function(event){
       event.preventDefault();
-      // var result = pigLatin(userInput);
+      var userInput = $("#input").val();
+      var translatedInput = pigLatin(userInput);
+      $("#result").text(translatedInput);
+    });
+});
 
-      $("#result").text(pigLatin);
-
-
-var pigLatin = [];
-var vowels = ("aeiou");
-var userInput = $("#input").val();
+var pigLatin = function(phraseToTranslate){
+  var vowels = ("aeiou");
+  for (var i = 0; i < vowels.length ; i++ ) {
+    for (var j = 0; j < phraseToTranslate.length ; i++ ) {
+      if ( phraseToTranslate.charAt(0) === vowels.charAt(i) ) {
+        var newPhrase = phraseToTranslate + "ay";
+        console.log(newPhrase);
+        break;
+      } else if ( phraseToTranslate.charAt(1) === vowels.charAt(i) ) {
+        var newPhrase = phraseToTranslate.slice(1) + phraseToTranslate.charAt(0) + "ay" ;
+        console.log(newPhrase);
+        break;
+      }
+      break;
+    }
+  }
+  return newPhrase;
+}
+// var pigLatin = [ ];
 
 // var makePigLatin = function(userInput){
-  for (var i = 0; i < vowels.length ; i++ ) {
-    for (var j = 0; j < userInput.length ; i++ ) {
-    if ( userInput.charAt(0) === vowels.charAt(i) ) {
-    pigLatin = userInput + "ay";
-    console.log(pigLatin);
-    break;
-    } else if ( userInput.charAt(1) === vowels.charAt(i) ) {
-      pigLatin = userInput.slice(1) + userInput.charAt(0) + "ay" ;
-      console.log(pigLatin);
-      break;
-    } break;
-  }
-}
 
-});
-});
 
 // var translatedPhrase = pigLatin(userInput);
 
