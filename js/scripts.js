f9d3de30cb766dec154980a8a22870ff676eb62f
Georgina Van Dort and Julia Dickey <julia@>
2016-06-08 15:06:11 -0700

    Make the second set of specs work

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,38 @@
 $(document).ready(function() {
 
+
     $("form#pig").submit(function(event){
       event.preventDefault();
-      var userInput = $("#input").val();
       // var result = pigLatin(userInput);
 
       $("#result").text(pigLatin);
-  });
-});
 
-var piglatin = [];
+
+var pigLatin = [];
 var vowels = ("aeiou");
+var userInput = $("#input").val();
 
-var makePigLatin = function(userInput){
-  for ( i = 0; i < userInput.length ; i++ ) {
-    for ( j = 0; j < vowels.length ; j++ ) {
-      if ((userInput.chatAt(i) === vowels[j]) && (i===0 )) {
-      pigLatin = userInput.concat("ay");
-      } else {
-        return false;
-      }
-    }
+// var makePigLatin = function(userInput){
+  for (var i = 0; i < vowels.length ; i++ ) {
+    for (var j = 0; j < userInput.length ; i++ ) {
+    if ( userInput.charAt(0) === vowels.charAt(i) ) {
+    pigLatin = userInput + "ay";
+    console.log(pigLatin);
+    break;
+    } else if ( userInput.charAt(1) === vowels.charAt(i) ) {
+      pigLatin = userInput.slice(1) + userInput.charAt(0) + "ay" ;
+      console.log(pigLatin);
+      break;
+    } break;
   }
 }
 
+});
+});
+
 // var translatedPhrase = pigLatin(userInput);
 
+// }
 
 
       // return i;
