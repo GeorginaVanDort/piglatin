2907d66f151471618b891103beed6ce8f98585f0
Georgina Van Dort and Julia Dickey <georgina@>
2016-06-08 11:40:21 -0700

    Add first spec to javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,25 @@
-
-
-
-
 $(document).ready(function() {
-
   $("form#pig").submit(function(event){
     event.preventDefault();
 
-    var year = ($("#input").val();
-    var result = leapYear(year);
+  var words = $("#input").val().split([""[String.length]]);
 
-    $("#result").text(result);
+  for (var i = 0; i < words.length; i++) {
+    if (words[0] === "a" || "e" || "i" || "o" || "u" ) {
+    result = words.concat("ay");
+    console.log(result);
+    break;
+    }
+    else {
+      alert("false");
+    }
+  }
+
+
+
+      // var result = pigLatin(words);
+
+      $("#result").text(result);
 
   });
 });
