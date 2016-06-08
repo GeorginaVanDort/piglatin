cb3868912cf97e80de8ca6e1ff41b3f3450f1652
Georgina Van Dort and Julia Dickey <julia@>
2016-06-08 14:13:49 -0700

    Make the first spec work in javascript with our js organized by logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,47 @@
 $(document).ready(function() {
-  $("form#pig").submit(function(event){
-    event.preventDefault();
 
-  var words = $("#input").val().split([""[String.length]]);
+    $("form#pig").submit(function(event){
+      event.preventDefault();
+      var userInput = $("#input").val();
+      // var result = pigLatin(userInput);
 
-  for (var i = 0; i < words.length; i++) {
-    if (words[0] === "a" || "e" || "i" || "o" || "u" ) {
-    result = words.concat("ay");
-    console.log(result);
-    break;
-    }
-    else {
-      alert("false");
-    }
-  }
-
-
-
-      // var result = pigLatin(words);
-
-      $("#result").text(result);
-
+      $("#result").text(pigLatin);
   });
 });
+
+var piglatin = [];
+var vowels = ("aeiou");
+
+var makePigLatin = function(userInput){
+  for ( i = 0; i < userInput.length ; i++ ) {
+    for ( j = 0; j < vowels.length ; j++ ) {
+      if ((userInput.chatAt(i) === vowels[j]) && (i===0 )) {
+      pigLatin = userInput.concat("ay");
+      } else {
+        return false;
+      }
+    }
+  }
+}
+
+// var translatedPhrase = pigLatin(userInput);
+
+
+
+      // return i;
+      // debugger;
+      // } else {
+      // return false;
+      // }
+
+// var pigLatin = function(input){
+//   for (var i = 0; i < words.length; i++) {
+//     if (words[0] === "a" || "e" || "i" || "o" || "u" ) {
+//     result = words.concat("ay");
+//     console.log(result);
+//     break;
+//     }
+//     else {
+//       alert("false");
+//     }
+//   }
